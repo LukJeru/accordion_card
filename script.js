3b9fc711ec87563b8aa453338ab46a1f8597a40c
Lukas Jerusalem <lukasjerusalem@web.de>
2023-08-06 20:09:02 +0200

    you can choose only one dropdown at the time

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ const func1 = (event) => {
   let para  = document.getElementById("paragraph_1");
   let heading = document.getElementById("heading_1");
   let div = document.getElementById("dropdown_1");
-  if (para.style.display === "none") {
+  if (para.style.display === "none" && document.querySelectorAll('.clicked').length === 0) {
     para.style.display = "block";
+    para.setAttribute("class", "clicked");
     heading.style.color = "hsl(237, 12%, 33%)";
     div.style.border = "none";
   } else {
     para.style.display = "none";
+    para.classList.remove("clicked");
     heading.style.color = "";
     div.style.border = "";
   };
 }
-
-drop1.addEventListener("click", func1)
 
 const drop2 = document.getElementById("dropdown_2");
 
@@ -23,18 +23,18 @@ const func2 = (event) => {
   let para  = document.getElementById("paragraph_2");
   let heading = document.getElementById("heading_2");
   let div = document.getElementById("dropdown_2");
-  if (para.style.display === "none") {
+  if (para.style.display === "none" && document.querySelectorAll('.clicked').length === 0) {
     para.style.display = "block";
+    para.setAttribute("class", "clicked");
     heading.style.color = "hsl(237, 12%, 33%)";
     div.style.border = "none";
   } else {
     para.style.display = "none";
+    para.classList.remove("clicked");
     heading.style.color = "";
     div.style.border = "";
   };
 }
-
-drop2.addEventListener("click", func2)
 
 const drop3 = document.getElementById("dropdown_3");
 
@@ -42,18 +42,18 @@ const func3 = (event) => {
   let para  = document.getElementById("paragraph_3");
   let heading = document.getElementById("heading_3");
   let div = document.getElementById("dropdown_3");
-  if (para.style.display === "none") {
+  if (para.style.display === "none" && document.querySelectorAll('.clicked').length === 0) {
     para.style.display = "block";
+    para.setAttribute("class", "clicked");
     heading.style.color = "hsl(237, 12%, 33%)";
     div.style.border = "none";
   } else {
     para.style.display = "none";
+    para.classList.remove("clicked");
     heading.style.color = "";
     div.style.border = "";
   };
 }
-
-drop3.addEventListener("click", func3)
 
 const drop4 = document.getElementById("dropdown_4");
 
@@ -61,18 +61,18 @@ const func4 = (event) => {
   let para  = document.getElementById("paragraph_4");
   let heading = document.getElementById("heading_4");
   let div = document.getElementById("dropdown_4");
-  if (para.style.display === "none") {
+  if (para.style.display === "none" && document.querySelectorAll('.clicked').length === 0) {
     para.style.display = "block";
+    para.setAttribute("class", "clicked");
     heading.style.color = "hsl(237, 12%, 33%)";
     div.style.border = "none";
   } else {
     para.style.display = "none";
+    para.classList.remove("clicked");
     heading.style.color = "";
     div.style.border = "";
   };
 }
-
-drop4.addEventListener("click", func4)
 
 const drop5 = document.getElementById("dropdown_5");
 
@@ -80,23 +80,22 @@ const func5 = (event) => {
   let para  = document.getElementById("paragraph_5");
   let heading = document.getElementById("heading_5");
   let div = document.getElementById("dropdown_5");
-  if (para.style.display === "none") {
+  if (para.style.display === "none" && document.querySelectorAll('.clicked').length === 0) {
     para.style.display = "block";
+    para.setAttribute("class", "clicked");
     heading.style.color = "hsl(237, 12%, 33%)";
     div.style.border = "none";
   } else {
     para.style.display = "none";
+    para.classList.remove("clicked");
     heading.style.color = "";
     div.style.border = "";
   };
 }
 
-drop5.addEventListener("click", func5)
 
-let arr =[];
-arr.push(document.getElementById("paragraph_1"));
-arr.push(document.getElementById("paragraph_2"));
-arr.push(document.getElementById("paragraph_3"));
-arr.push(document.getElementById("paragraph_4"));
-arr.push(document.getElementById("paragraph_5"));
-let found = arr.find((element) => element.style.display === "none")
+drop1.addEventListener("click", func1);
+drop2.addEventListener("click", func2);
+drop3.addEventListener("click", func3);
+drop4.addEventListener("click", func4);
+drop5.addEventListener("click", func5);
